e63fa4a1b7141e47e5d832808e5222c67fdb0cbd
borysbudner <borysbudner@gmail.com>
2020-09-14 14:13:18 +0200

    Add visibility of menu

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,12 @@ const menuButton = document.getElementById("menu-button");
 
 function openNav() {
     menu.style.width = "70%";
+    menu.style.visibility = "visible";
 }
 
 function closeNav() {
     menu.style.width = "0";
+    menu.style.visibility = "hidden";
 }
 
 window.addEventListener('click', function(event){
